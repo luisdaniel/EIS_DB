75629c0e2516d96de7076aeb50fb87d14a29b5e2
luisdaniel <luisdaniel@gmail.com>
2014-11-20 11:41:11 -0500

    change host and stuff

--- utils.py
+++ utils.py
@@ -19,12 +19,22 @@ ES_HOST=os.environ.get('ES_HOST')
 host_params = {'host':os.environ.get('ES_HOST'), 'port':80, 'use_ssl':False}
 es = ES([host_params], connection_class=RC, http_auth=(ES_USER, ES_PASS),  use_ssl=False)
 
+states ={"AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", 
+	"DC": "District of Columbia", "FL": "Florida", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KA": "Kansas", 
+	"KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", 
+	"MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", 
+	"NC": "North Carolina", "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island", "SC": "South Carolina", 
+	"SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", 
+	"WI": "Wisconsin", "WY": "Wyoming", "PR": "Puerto Rico"}
+
+
+
 class Search(object):
 	def search(self, query, from_, size):
 		res = es.search(index="eis", body={
 			"query": {
 				"query_string":{
-					"fields":['title'],
+					"fields":['title', 'state', 'agency'],
 					"query":query
 				}
 			}
