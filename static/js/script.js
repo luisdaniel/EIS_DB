d55c50cbf809029a036cf2652d62e522e13bf6a9
luisdaniel <luisdaniel@gmail.com>
2015-01-05 13:40:53 -0500

    dropdown and single search

--- static/js/script.js
+++ static/js/script.js
@@ -17,5 +17,16 @@ $(document).ready(function() {
     $('.report-filters').on('click', '#clear-filters', function() {
         window.location.replace("/");
     });
+    console.log($(".filter-arrow").text())
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+      if ($('.filter-arrow').text() == '▶') {
+        $('.filter-arrow').text('▼');
+      } else {
+        $('.filter-arrow').text('▶');
+      }
+    });
 
 });
